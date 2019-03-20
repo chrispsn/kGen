982f18b2df585481e2d957197890842007cc8d32
Chris Pearson <chrispsn@users.noreply.github.com>
2019-03-20 20:16:50 +1100

    Create shakti.py

---
+++ shakti.py
@@ -0,0 +1,26 @@
+from shakti import k, error
+from itertools import permutations
+
+# !\'.
+chars = "@#$%^&*()-_=+[]{};:\",<>?|/"
+
+bad_substr = {}
+
+# TODO write input in Python and serialise to k
+tests = [
+    {"i": "1 2 3", "o": 6},
+    {"i": "1 2 3 4", "o": 10},
+]
+
+progs = ("".join(p) + " " for p in permutations(chars, 2))
+valid_progs = (p for p in progs if all(x not in p for x in bad_substr))
+
+worked = []
+for p in valid_progs:
+    try:
+        if all(t["o"] == k(p+t["i"]) for t in tests):
+            worked.append(p)
+    except error:
+        pass
+        
+print("Worked:", worked)
