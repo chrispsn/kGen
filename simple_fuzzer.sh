d302761ea504b007120c3f065690826a8882ec19
Chris Pearson <chrispsn@users.noreply.github.com>
2019-03-23 16:44:06 +1100

    Create simple_fuzzer.sh

---
+++ simple_fuzzer.sh
@@ -0,0 +1,11 @@
+> log.txt
+for f in in/*; do
+    echo "$f";
+    for i in {1..1000000}; do
+        # Do not send in non-ASCII characters:
+        # http://www.linuxcertif.com/man/1/zzuf/
+        zzuf --seed $i --ratio 0.1 --max-crashes 0 \
+                --refuse '\x00-\x1f\x7f-\xff' \
+        cat "$f" | timeout -k 1 1 ./k &>>log.txt
+    done
+done
